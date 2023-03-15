f6da55848b608631f2d3024ab5d7bc0d181169d4
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-03-15 15:39:33 +0700

    feat: change api, set up pagination

--- Pagination-table/js/main.js
+++ Pagination-table/js/main.js
@@ -1,13 +1,87 @@
-// "id", "email", "first_name", "last_name", "avatar"
+userExample = {
+  id: 1,
+  firstName: "Terry",
+  lastName: "Medhurst",
+  maidenName: "Smitham",
+  age: 50,
+  gender: "male",
+  email: "atuny0@sohu.com",
+  phone: "+63 791 675 8914",
+  username: "atuny0",
+  password: "9uQFF1Lh",
+  birthDate: "2000-12-25",
+  image: "https://robohash.org/hicveldicta.png",
+  bloodGroup: "A−",
+  height: 189,
+  weight: 75.4,
+  eyeColor: "Green",
+  hair: { color: "Black", type: "Strands" },
+  domain: "slashdot.org",
+  ip: "117.29.86.254",
+  address: {
+    address: "1745 T Street Southeast",
+    city: "Washington",
+    coordinates: { lat: 38.867033, lng: -76.979235 },
+    postalCode: "20020",
+    state: "DC",
+  },
+  macAddress: "13:69:BA:56:A3:74",
+  university: "Capitol University",
+  bank: {
+    cardExpire: "06/22",
+    cardNumber: "50380955204220685",
+    cardType: "maestro",
+    currency: "Peso",
+    iban: "NO17 0695 2754 967",
+  },
+  company: {
+    address: {
+      address: "629 Debbie Drive",
+      city: "Nashville",
+      coordinates: { lat: 36.208114, lng: -86.58621199999999 },
+      postalCode: "37076",
+      state: "TN",
+    },
+    department: "Marketing",
+    name: "Blanda-O'Keefe",
+    title: "Help Desk Operator",
+  },
+  ein: "20-9487066",
+  ssn: "661-64-2976",
+  userAgent:
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/12.0.702.0 Safari/534.24",
+};
 
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let pageNumbers = 7;
-let userPerPageNumbers = 7;
-let columns = ["id", "email", "first_name", "last_name"];
+let pageNumbers = 10;
+let userPerPageNumbers = 10;
+let columns = [
+  "id",
+  "firstName",
+  "lastName",
+  "maidenName",
+  "age",
+  "gender",
+  "email",
+  "phone",
+  "username",
+  "birthDate",
+  "height",
+  "weight",
+  "domain",
+  "ip",
+  "address.city",
+  "address.postalCode",
+];
+
+let skip = 0;
+let usersData = [];
+
+let currentActivePage = 1;
 
 function createTable({ rows, cols }) {
   const mainCont = document.getElementById("main-cont__table");
@@ -57,7 +131,7 @@ function createPagination() {
 function createArrow(symb, name) {
   let cell = document.createElement("div");
   cell.className = `pagination_arrow__${name}-item`;
-  
+
   let arrowLeft = document.createElement("a");
   arrowLeft.className = `pagination_arrow__${name}-link`;
   arrowLeft.innerText = symb;
@@ -91,12 +165,31 @@ window.onload = function () {
   });
   createPagination(pageNumbers);
 
-  createList();
+  createTableList();
+
+  document
+    .querySelectorAll("div.pagination_item")
+    .forEach((item) => document.addEventListener('click', changePage));
 };
 
-function createList() {
+function changePage(e) {
+  changeActiveButton(e.target.id);
+
+  getUsers(userPerPageNumbers, (currentActivePage-1)*userPerPageNumbers);
+}
+
+function changeActiveButton(id) {
+  let activeElement = document.getElementById(`pagination_link ${currentActivePage}`);
+  activeElement.classList.remove('active');
+
+  currentActivePage = +id.split(' ').at(-1);
+  let newActiveElement = document.getElementById(`pagination_link ${currentActivePage}`);
+  newActiveElement.classList.add('active');
+}
+
+function createTableList() {
   fillHeader();
-  fillTable();
+  getUsers(userPerPageNumbers, skip);
 }
 
 function fillHeader() {
@@ -109,29 +202,37 @@ function fillHeader() {
   }
 }
 
-// {"data": {
-//     "id": 1,
-//     "email": "george.bluth@reqres.in",
-//     "first_name": "George",
-//     "last_name": "Bluth",
-//     "avatar":"https://reqres.in/img/faces/1-image.jpg",
-// }}
-
 // https://dummyjson.com/users
 
-async function getUser(r) {
+async function getUsers(limit, skip) {
+  usersData = [];
   try {
-    const response = await fetch(`https://reqres.in/api/users/${r}`);
+    const response = await fetch(
+      `https://dummyjson.com/users?limit=${limit}&skip=${skip}`
+    );
     const responseData = await response.json();
-    console.log(responseData);
-    for (let c = 0; c < columns.length; c++) {
-      const cellText = document.getElementById(
-        `table-cell__text ${r + 1}-${c + 1}`
-      );
-      cellText.innerText = responseData["data"][columns[c]];
+    for (let user of responseData.users) {
+      usersData.push(user);
     }
+
+    fillTable();
+
   } catch (error) {
     console.error(error);
+  }
+}
+
+function getUser(r) {
+  for (let c = 0; c < columns.length; c++) {
+    const cellText = document.getElementById(
+      `table-cell__text ${r + 1}-${c + 1}`
+    );
+    if (columns[c].includes(".")) {
+      let prop = columns[c].split(".");
+      cellText.innerText = usersData[r - 1][prop[0]][prop[1]];
+    } else {
+      cellText.innerText = usersData[r - 1][columns[c]];
+    }
   }
 }
 
